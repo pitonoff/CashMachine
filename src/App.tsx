24498680924b7d642077f6bd510a1d236f664ad3
Alexander Moshnitsky <pitonthesadboy@gmail.com>
2023-09-21 10:34:25 +0400

    added a pre-loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,38 @@
-import './index.css'
+import React, { useState, useEffect } from 'react';
+import './index.css';
 import Counter from './components/Counter';
 import SubscribeBlock from './components/SubscribeBlock';
-import './BanknoteAnimation.css'
+import './BanknoteAnimation.css';
+import Preloader from './Preloader';
 
-function App() {
+const App: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const svgLoader = setTimeout(() => {
+      // Simulate SVG loading time (e.g., 3 seconds)
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(svgLoader);
+  }, []); 
+
   return (
     <div>
-      <div className="background-container">
-        <div className="background" />
-      </div>
-      <div className="content">
-        <Counter />
-        <SubscribeBlock />
-      </div>
+      {loading ? (
+        <Preloader />
+      ) : (
+        <div>
+          <div className="background-container">
+            <div className="background" />
+          </div>
+          <div className="content">
+            <Counter />
+            <SubscribeBlock />
+          </div>
+        </div>
+      )}
     </div>
- 
   );
 }
 
---
+++ src/Preloader.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Preloader.css';
+
+const Preloader: React.FC = () => {
+  return (
+    <div className="preloader">
+      <div className="loader" />
+    </div>
+  );
+};
+
+export default Preloader;
